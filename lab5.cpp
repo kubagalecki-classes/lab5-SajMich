7dd46d95d3b716a97eaea3a08a80563453c41358
SajMich <michal.sajewicz.stud@pw.edu.pl>
2020-12-03 11:03:06 +0000

    solved_v1

--- lab5.cpp
+++ lab5.cpp
@@ -1,6 +1,52 @@
+#include "Human.hpp"
 #include <iostream>
+#include <list>
+#include <vector>
+
+std::vector< char > foo(std::list< Human >& people)
+{
+    std::vector< char >          tmp;
+    std::list< Human >::iterator it = people.begin();
+    // std::cout << it->getName() << "\n";
+    int i = 2;
+    for (int j = 0; j < i; j++) {
+        std::cout << it->getName() << "\n";
+        tmp.push_back('y');
+        *it++;
+    }
+    return {tmp};
+}
 
 int main()
 {
-    puts("Hello, World!");
+    std::list< Human > people;
+    // std::list< Human >::iterator it = people.begin();
+    people.push_back(Human("Mark", 20, false, true));
+    people.push_back(Human("John", 26, false, true));
+    /*
+    *it++;
+    std::cout << it->getName() << "\n";
+*/
+    std::vector< char > wektor;
+    wektor = foo(people);
+    for (int i = 0; i < 2; i++) {
+        std::cout << wektor[i] << "\n";
+    }
+    /*
+       std::list< int >           L;
+       std::list< int >::iterator it = L.end();
+       L.push_back(10);
+       L.push_front(1);
+       L.push_front(1);
+       L.push_front(1);
+       L.push_front(1);
+       L.push_front(1);
+       int i = 0;
+       // std::cout << "rozmiar listy: " << *it++ << "\n";
+       while (it != L.begin() && i < 100) {
+           std::cout << *it << "\n";
+           *it--;
+           i++;
+       }
+       */
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -7,6 +7,21 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+  std::vector<char> tmp;
+  std::list< Human >::iterator it = people.begin();
+  int iter =0;
+while( it != people.end() && iter <1000)
+{
+  it->birthday();
+  if ( it-> isMonster())
+  {
+    tmp.push_back('y');
+  }
+  else 
+  {
+    tmp.push_back('n');
+  }
+  *it++
+}
+    return {tmp};
 }
